4e9ac49477da6632fac3ffc6c49539ed77fc7f9f
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-02-26 01:49:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+//package nptell;
+public class Main {
+        public static void main(String[] args) {
+                System.out.println("hello world");
+                int arr;
+//                int 1arr;
+                int st=5;
+                int ST=6;
+//                int void;
+//                int Void;not a good practice
+//                int st ud ent ;not allowed
+                int stUdEnt;
+                int num1 =3;
+                int num2 = 5;
+                int num3 = 7;
+                int sum = num1+ num2+ num3;
+                System.out.println(sum);
+        }
+}
