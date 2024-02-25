d7b641d82bc40cac0098935b885724ed524def26
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-02-26 01:50:56 +0530

    Add files via upload

---
+++ CWH07_PracticeSet1.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class CWH07_PracticeSet1
+{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+   /*
+        WAP to sum 3 numbers in JAVA
+
+        System.out.println("Enter The Value Of  a :");
+        int a = s.nextInt();
+        System.out.println("Enter The Value Of  b :");
+        int b= s.nextInt();
+        System.out.println("Enter The Value Of  c :");
+        int c= s.nextInt();
+        int sum = a+b+c;
+        System.out.println("The sum of a&b&c is : "+ sum);
+
+    */
+
+/*        Question 2 :
+        Calculate CGPA of 3 subjects out of 100
+
+        float sub1 = 55.00f;
+        float sub2 = 66.00f;
+        float sub3 = 98.0f;
+        float sum = sub1+sub2+sub3;
+        float CGPA = sum/30f;
+        System.out.println(CGPA+"CGPA");
+
+ */
+        /* Question 3
+        take an input name and print => hello "name " have a good day
+
+        System.out.println("what is your name ?");
+        String name = s.next();
+        System.out.println("Hello "+ name + " have a good day .");
+          */
+
+        /*
+            Question 4
+            convert kilometers to miles
+
+        System.out.println("enter the kilometer value to be converted into miles : ");
+        float kilometers = s.nextFloat();
+        float miles = kilometers*0.621371f;
+        System.out.println("the miles value of "+kilometers +" will be : "+miles+" miles. ");
+         */
+
+        /*
+        Question 5
+        detect if a number entered by user is integer or not ?
+
+        System.out.println("enter the number to detect if it is integer or not : ");
+        boolean num = s.hasNextInt();
+        System.out.println(num);
+         or
+        System.out.println("enter the number to detect if it is integer or not : ");
+        System.out.println(s.hasNextInt());
+
+         */
+    }
+}
