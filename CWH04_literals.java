010cf1a491bd6c14c3197af9d984fab16405c173
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-02-26 01:49:37 +0530

    Add files via upload

---
+++ CWH04_literals.java
@@ -0,0 +1,15 @@
+public class CWH04_literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        System.out.println(age);
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        long ageDyno =566666666666L;
+        int age1=33;
+        short age3 =83;
+        boolean a =true;
+        String AA = "Shruti";
+        System.out.println(AA);
+    }
+}
