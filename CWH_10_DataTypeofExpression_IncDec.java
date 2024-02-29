3db160ff928fc50a3d38f67d1c92dd9b07e0abda
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-02-29 23:58:11 +0530

    Add files via upload

---
+++ CWH_10_DataTypeofExpression_IncDec.java
@@ -0,0 +1,25 @@
+public class CWH_10_DataTypeofExpression_IncDec {
+    public static void main(String[] args) {
+       /*
+        int a =45+4;
+        float v = 6.54f;
+        byte x = 5;
+        int y= 6;
+        short z = 8;
+        int b = y+z;
+        float c = v+x;
+        b + s = int         b=byte  s=short  s = short  d= double
+        s+i = int            i = int  c=char  l = long
+        L+F = float
+        i+f = float
+        c+i = int
+        c+s = int
+        l+d =double
+        f+d = double
+        System.out.println(c);
+         */
+
+        //Increment & Decrement Operator
+        
+    }
+}
