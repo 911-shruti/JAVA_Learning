9b3645018f5f087d04018b24a020abc82fd9c70b
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-04-08 21:40:54 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,36 @@
-//package nptell;
+package SSK;
+
 public class Main {
-        public static void main(String[] args) {
-                System.out.println("hello world");
-                int arr;
-//                int 1arr;
-                int st=5;
-                int ST=6;
-//                int void;
-//                int Void;not a good practice
-//                int st ud ent ;not allowed
-                int stUdEnt;
-                int num1 =3;
-                int num2 = 5;
-                int num3 = 7;
-                int sum = num1+ num2+ num3;
-                System.out.println(sum);
-        }
+    public static void main(String[] args) {
+//        System.out.println();
+//        OOP_02KK kunal = new OOP_02KK(21,33000,"Kunal",false);
+//        OOP_02KK Shruti = new OOP_02KK(21,300,"Shruti",false);
+//        System.out.println(kunal.name+" "+kunal.age+" "+kunal.salary+" "+kunal.married);
+//        System.out.println(Shruti.name+" "+Shruti.age+" "+Shruti.salary+" "+Shruti.married);
+//        System.out.println(kunal.population);
+
+
+       // greeting();
+        fun();
+
+//        Main obj = new Main();
+//        obj.greeting();
+
+        Main f = new Main();
+        f.greeting();
+
+    }
+    static void fun(){
+        //this is not dependent to an object
+        System.out.println("i'm bored but can't be bored");
+       // greeting();//can't use this becouse it requires an object
+        //but the function you are using it in does not depend on instance
+    }
+    void greeting(){
+        //we know that something which is not static, belongs to an object
+        System.out.println("i'm hungry");
+    }
+    void fun2(){
+        greeting();
+    }
 }
