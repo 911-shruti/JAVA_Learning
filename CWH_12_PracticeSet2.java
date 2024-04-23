c5934b652cdf6bfc66b7b2b6b230f617c6c0721b
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-04-23 18:25:16 +0530

    Add files via upload

---
+++ CWH_12_PracticeSet2.java
@@ -0,0 +1,37 @@
+package CWH;
+import java.util.Scanner;
+public class CWH_12_PracticeSet2 {
+    public static void main(String[] args) {
+        float a = 7/4f*9/2f;
+        System.out.println(a);
+
+        //WAP to encrypt a grade by adding 8 to it,decrypt it to show the correct grade
+       //incryption
+//        char grade = 'B';
+//        grade = (char)(grade + 8);
+//        System.out.println(grade);
+//
+//        //decryption
+//        grade = (char)(grade-8);
+//        System.out.println(grade);
+
+
+
+/*Use a comparison operator to find out wether a given number
+ is greater than the user entered or not
+        Scanner S = new Scanner(System.in);
+        int x = S.nextInt();
+        System.out.println(x>8);
+
+ */
+        Scanner sc =new Scanner(System.in);
+        int v = sc.nextInt();
+        int u = sc.nextInt();
+        System.out.println((v*v-u*u)/295f);
+        char b ='a';
+        System.out.println((int)(a+3));
+
+
+
+    }
+}
