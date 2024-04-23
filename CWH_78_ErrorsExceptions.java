ef3aef32345d18ea802579ddaddb7cee7b65337c
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-04-23 18:37:04 +0530

    Add files via upload

---
+++ CWH_78_ErrorsExceptions.java
@@ -0,0 +1,30 @@
+package CWH;
+public class CWH_78_ErrorsExceptions {
+    public static void main(String[] args) {
+        System.out.println("This is Errors & Exceptions in Java");
+        /*
+        Error: the problems in the program
+                     or
+         whenever we see some issue with the program, and it doesn't work as expected.
+             3 Types
+             1>. Syntax error: while writing a program, something wrong in syntax. or
+              when the compiler finds something wrong with the program it throws some error.
+             i.e., int a=5;
+                  System.out.println(a+b);
+                  d=5;
+             2>. Logical error/bug: if the program doesn't work as we expected
+                  i,e in a chatting app, the message delivered to the wrong address
+                   or on clicking some button the website throws us into unknown
+                    website or outside the site.
+             3>. Runtime error(also called exception): when the program shows or gets error
+                                   while running it may be when a user enters some wrong input
+             we mostly use exception handling to let our users know the reason behind
+             the issue they face at runtime.
+
+             the runtime error is mostly faced at a user end & the Syntax and logical error
+             are faced at a programmer end.
+
+         */
+
+    }
+}
