86ff91d61b71f15c48ddb68bc7fc1dba7a92c8d8
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-04-23 18:37:40 +0530

    Add files via upload

---
+++ CWH_80_Try_Catch_Block.java
@@ -0,0 +1,9 @@
+package CWH;
+public class CWH_80_Try_Catch_Block {
+    public static void main(String[] args){
+        System.out.println( "dsdf");
+    }
+
+    // an exception is an event that occurs when the program is executed disrupting thr normal flow of instruction
+
+}
