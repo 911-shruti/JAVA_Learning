a8a8d6a5cd415555bcbea526554cb042b0b7560b
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2024-04-23 18:29:32 +0530

    Add files via upload

---
+++ CWH_51_Exercise4.java
@@ -0,0 +1,13 @@
+package CWH;
+
+public class CWH_51_Exercise4 {
+    public static void main(String[] args) {
+        /*
+        1. we have to impliment a library using java class library
+        functions: issueBook(), returnBook() ,showAvailabbleBook() ,addBook()
+        properties : 1 Array to store the available book ,
+        1 array to store issued book
+
+         */
+    }
+}
